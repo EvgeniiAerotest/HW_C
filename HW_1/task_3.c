fe93fabea7a98832d7c0a3d1bcff43ff1ebca1c7
EvgeniiAerotest <zakutskii@mail.ru>
2024-10-23 20:22:35 +0300

    task_3 commit

---
+++ HW_1/task_3.c
@@ -0,0 +1,19 @@
+/*
+ * task_3.c
+ *  
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf("*******\n");
+	printf(" HHOHH\n");
+	printf(" ZZZZZ\n");
+	return 0;
+}
+
