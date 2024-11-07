875bd06195e15969d585483052100abcb2596b3b
EvgeniiAerotest <zakutskii@mail.ru>
2024-11-08 00:55:38 +0300

    HW_4 task_6

---
+++ HW_4/task_6.c
@@ -0,0 +1,29 @@
+/* HW4
+ * task_6.c
+ *  
+ */
+
+
+#include <stdio.h> 
+int main(void)
+{
+ int a, b; 
+  scanf("%d%d", &a, &b);
+  if (a==b)
+   {
+	printf("Equal");  
+   } 
+  else if (a>b) 
+   {
+	printf("Above");  
+   }
+  else 
+   {
+	printf("Less");  	   
+   }
+ return 0; 
+}
+
+
+ 
+	
