e88c8aa2c489407fbeacf1c6aba3245f66116320
EvgeniiAerotest <zakutskii@mail.ru>
2024-11-08 00:54:03 +0300

    HW_4 task_2

---
+++ HW_4/task_2.c
@@ -0,0 +1,20 @@
+/* HW4
+ * task_2.c
+ *  
+ */
+
+
+#include <stdio.h> 
+int main(void)
+{
+ int a, b, c, max;
+  scanf("%d%d%d", &a, &b, &c); 
+  max =  a > b ?  a : b;
+  max = max> c ? max: c;
+  printf("%d", max);
+ return 0; 
+}
+
+
+
+	
