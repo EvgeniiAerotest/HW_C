9e04cd96a8fa64641c91270a2199f5cbe19f9bf4
EvgeniiAerotest <zakutskii@mail.ru>
2024-11-08 00:52:59 +0300

    HW_4 task_1

---
+++ HW_4/task_1.c
@@ -0,0 +1,19 @@
+/* HW4
+ * task_1.c
+ *  
+ */
+
+
+#include <stdio.h> 
+int main(void)
+{
+ int a, b; 
+ scanf ("%d""%d", &a,&b); 
+ (a >= b) ? printf("%d  %d", b,a) : printf("%d  %d", a,b);
+ return 0;
+  
+}
+
+
+
+	
