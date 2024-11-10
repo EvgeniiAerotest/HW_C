0a0a02afab2dcf53ac155845a0f58bd7cb1538fb
EvgeniiAerotest <zakutskii@mail.ru>
2024-11-10 23:50:48 +0300

    HW_5 task_2

---
+++ HW_5/task_2.c
@@ -0,0 +1,17 @@
+/* HW5
+ * task_2.c
+ *
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	int a, b, sum=0;
+	scanf("%d%d", &a, &b);
+	for (int i = a; i <= b; i++)
+	{
+		sum+=i*i;
+	}
+	printf("%d", sum);
+	return 0;
+}
