f60dd4886cff3fdeb8d053b4df18cd8028cf4d3f
EvgeniiAerotest <zakutskii@mail.ru>
2024-11-12 22:34:13 +0300

    HW_5 task_5 v2

--- HW_5/task_5.c
+++ HW_5/task_5.c
@@ -7,19 +7,12 @@ int main(void)
 {
 	int a, dig;
 	int sum = 0;
-	int i = 0;
-	int arr[100];
 	scanf("%d", &a);
 	while (a != 0)
 	{
 		dig = a % 10;
-		arr[i] = dig;
-		i++;
+		sum += dig;
 		a = a / 10;
-	}
-	for (int k = 0; k < i; k++)
-	{
-		sum += arr[k];
 	}
 	printf("%d", sum);
 	return 0;
