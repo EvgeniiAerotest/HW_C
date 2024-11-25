79749d4ec2e9507368535e6a13e0b894fd39a790
EvgeniiAerotest <zakutskii@mail.ru>
2024-11-25 18:44:19 +0300

    HW_7 task_6.c

---
+++ HW_7/task_6.c
@@ -0,0 +1,27 @@
+/* HW7
+ * task_6.c
+ *
+ */
+#include <stdio.h>
+ 
+void print_rev (void);
+
+int main(void)
+{
+ print_rev();
+ return 0;
+}
+
+
+void print_rev (void)
+{
+ char ch;
+ scanf ("%c", &ch); 
+ if (ch != '.')
+ {
+   print_rev (); 
+   printf ("%c", ch); 
+ }
+} 
+
+
