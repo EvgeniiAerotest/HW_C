ba17e316452ba94c502827e7108251a6554067ec
EvgeniiAerotest <zakutskii@mail.ru>
2024-11-25 18:27:07 +0300

    HW_7 task_2.c

---
+++ HW_7/task_2.c
@@ -0,0 +1,23 @@
+/* HW7
+ * task_2.c
+ *
+ */
+ 
+int SumRec(int n);
+
+#include <stdio.h>
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	printf("%d ", SumRec(n));
+    return 0;
+}
+
+
+int SumRec(int n)
+{
+	if(n>0)
+	  n = n+SumRec(n-1);
+	return n;
+}
