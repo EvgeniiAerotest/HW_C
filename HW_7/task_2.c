4d462a199666c6f4118a1150eedccab2fb463091
EvgeniiAerotest <zakutskii@mail.ru>
2024-11-25 18:34:03 +0300

    HW_7 task_2.c c#2

--- HW_7/task_2.c
+++ HW_7/task_2.c
@@ -8,16 +8,16 @@ int SumRec(int n);
 #include <stdio.h>
 int main(void)
 {
-	int n;
-	scanf("%d", &n);
-	printf("%d ", SumRec(n));
-    return 0;
+ int n;
+ scanf("%d", &n);
+ printf("%d ", SumRec(n));
+ return 0;
 }
 
 
 int SumRec(int n)
 {
-	if(n>0)
-	  n = n+SumRec(n-1);
-	return n;
+ if(n>0)
+   n = n+SumRec(n-1);
+ return n;
 }
