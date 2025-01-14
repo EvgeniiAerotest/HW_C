a3863ac708a3e52d844080e1baf80c25d46495a6
EvgeniiAerotest <zakutskii@mail.ru>
2025-01-14 23:11:29 +0300

    temp_api.h commit#2

--- CP/temp_api.h
+++ CP/temp_api.h
@@ -5,21 +5,21 @@
 
 #define SIZE 600000
 
-struct sensor {
+struct sensor
+{
     uint16_t year;
     uint8_t month;
     uint8_t day;
     uint8_t hour;
-    uint8_t minute;    
+    uint8_t minute;
     int8_t t;
 };
 
-// Прототипы функций
-void SortByT(struct sensor* info, int n);
-void SortByDate(struct sensor* info, int n);
-int AddInfo(struct sensor* info);
-void print(struct sensor* info, int number);
-float TempAvrM(struct sensor* info, int n, uint16_t year, uint8_t month);
+void SortByT(struct sensor *info, int n);
+void SortByDate(struct sensor *info, int n);
+int AddInfo(struct sensor *info);
+void print(struct sensor *info, int number);
+float TempAvrM(struct sensor *info, int n, uint16_t year, uint8_t month);
 int TempMinM(struct sensor *info, int n, uint16_t year, uint8_t month);
 int TempMaxM(struct sensor *info, int n, uint16_t year, uint8_t month);
 int TempMinY(struct sensor *info, int n);
@@ -30,7 +30,7 @@ void print_all_monthes(struct sensor *info, int data_count);
 void print_one_month(struct sensor *info, int data_count, int month_int);
 void print_year(struct sensor *info, int data_count);
 int Data_Month_Count(struct sensor *info, int n, uint16_t year, uint8_t month);
-int load_data_file(char* filename,struct sensor *info);
+int load_data_file(char *filename, struct sensor *info);
+void print_help(void);
 
 #endif
-
